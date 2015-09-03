a84fe5e9eadb86d3f06466817942cb878164d50a
benjamindartigues <bdartigues@gmail.com>
2015-09-03 16:03:01 +0200

    adding new result page

--- src/result_page.php
+++ src/result_page.php
@@ -653,6 +653,7 @@ echo'<div id="summary">
 
                     </div>
                 </div>
+                <div id="shift_line"></div>
             </div>';
                   //$protein="Q39255";   
                   
