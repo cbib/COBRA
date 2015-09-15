933fb29732d0c0fe5a24a8db0ccc7df043d696aa
benjamindartigues <bdartigues@gmail.com>
2015-09-15 17:50:43 +0200

    adding new result page

--- src/result_page.php
+++ src/result_page.php
@@ -185,7 +185,7 @@ if (((isset($_GET['organism'])) && ($_GET['organism']!='')) && ((isset($_GET['se
 echo'<div id="summary">   
         <div id="protein-details">
             <div id="details">
-            <div id="section_description"'.$gene_id[0].'
+            <div id="section_description">'.$gene_id[0].'
             <div id="organism" class="right"><h4>'.$species.'</h4></div>';
                 echo '<h1>';
                 for ($i = 0; $i < count($gene_symbol); $i++) {
