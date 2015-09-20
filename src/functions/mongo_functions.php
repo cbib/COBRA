4214b1116b2def76980aee1ee6810b2407a943f2
benjamindartigues <bdartigues@gmail.com>
2015-09-20 11:18:28 +0200

    adding new result page

--- src/functions/mongo_functions.php
+++ src/functions/mongo_functions.php
@@ -883,10 +883,10 @@ function get_ortholog(MongoCollection $mappingsCollection, Mongocollection $orth
              //   foreach ($initial_species as $key => $value) {
              //       if ($value==$ortholog[0].$ortholog[1] && $ortholog[2]!='R'){
                         #echo "start line : ".$buffer."\n";
-                $cursors=$mappingsCollection->find(array('mapping_file.Plaza ID'=>$ortholog),array('mapping_file.$'=>1,'species'=>1,'_id'=>0));
-                foreach ($cursors as $cursor){
-                    foreach ($cursor as $go){
-                        foreach ($go as $value){
+                $ortholog_data=$mappingsCollection->find(array('mapping_file.Plaza ID'=>$ortholog),array('mapping_file.$'=>1,'species'=>1,'_id'=>0));
+                foreach ($ortholog_data as $data){
+                    foreach ($data as $key){
+                        foreach ($key as $value){
                             echo $value['species'];
                             
 //                    $table_string.="<tr>";
