cb59b63d77ffa2ad547ef3a5f1ab31c88e404e80
benjamindartigues <bdartigues@gmail.com>
2015-09-21 23:32:22 +0200

    adding new result page

--- src/orthology/ortholog_search.php
+++ src/orthology/ortholog_search.php
@@ -125,12 +125,12 @@ $Topgene=$_POST['Topgene'];
 //$species='Hordeum vulgare';
 
 foreach ($data as $species){
-    echo $species;
+    echo 'species: '.$species;
     if ($species=="Prunus species"){
         
         $species_id_type=$speciesCollection->find(array('classification.genus'=>'Prunus'),array('preferred_id'=>1,'full_name'=>1));
         foreach ($species_id_type as $value) {
-            echo $value['full_name'];
+            echo 'full name: '.$value['full_name'];
             //$gene_list_attributes=get_ortholog_list_2($mappingsCollection,$measurementsCollection,$speciesCollection,$value['full_name'],$genes,$Topgene);
             $gene_list_attributes=get_ortholog_list($mappingsCollection,$measurementsCollection,$speciesCollection,$species,$genes,$Topgene);
 
@@ -142,7 +142,7 @@ foreach ($data as $species){
     else{
         echo '<H1>Results for '.$species.'</H1>';
 
-        $type=$genes;
+        //$type=$genes;
 
         $gene_list_attributes=get_ortholog_list($mappingsCollection,$measurementsCollection,$speciesCollection,$species,$genes,$Topgene);
 
