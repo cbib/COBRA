6cf581cbb9e65cee431276f5537472109c4ef261
benjamindartigues <bdartigues@gmail.com>
2015-09-23 15:39:51 +0200

    adding new result page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+//session_start();
 // on teste si le visiteur a soumis le formulaire de connexion
 //if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
 //	if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
