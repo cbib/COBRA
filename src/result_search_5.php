7a292606c70d7528be74a8e3ced7c711b7b58c36
benjamindartigues <bdartigues@gmail.com>
2015-09-25 11:27:54 +0200

    adding new result page

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -258,7 +258,7 @@ echo   '<div id="summary">
 //                        
 //                    }
 //                      
-                    var_dump($value);
+                    var_dump($result);
                 }
                
                 echo'
