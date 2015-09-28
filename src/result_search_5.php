3e36b242b6cfc49768bd1a11497433fc6108bd87
benjamindartigues <bdartigues@gmail.com>
2015-09-28 14:32:33 +0200

    adding new result page

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -1314,7 +1314,7 @@ new_cobra_footer();
 
 
 <script type="text/javascript" class="init">
-    var species="<?php echo '$species'; ?>"; 
+    var species="<?php echo $species; ?>"; 
     $(function(){
     $('#container').highcharts({
         chart: {
