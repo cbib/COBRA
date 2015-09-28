1cf8f825a97e7953cade8e37550d2e71f28c55aa
benjamindartigues <bdartigues@gmail.com>
2015-09-28 16:33:33 +0200

    adding new result page

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -1356,7 +1356,26 @@ new_cobra_footer();
         credits: {
             enabled: false
         },
-        series: serie
+        series: [{
+            name: 'condition 1',
+            data: [5]
+        }, {
+            name: 'condition 2',
+            data: [2]
+        }, {
+            name: 'condition 3',
+            data: [3]
+        },{
+            name: 'condition 4',
+            data: [2]
+        },{
+            name: 'condition 5',
+            data: [2]
+        },{
+            name: 'condition 6',
+            data: [2]
+        },]
+        //series: serie
     });
 });
 	$(document).ready(function() {
