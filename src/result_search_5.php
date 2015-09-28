a196d8a327df5bb909461a5d9b92d9c89afbac24
benjamindartigues <bdartigues@gmail.com>
2015-09-28 14:38:58 +0200

    adding new result page

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -1315,66 +1315,29 @@ new_cobra_footer();
 
 <script type="text/javascript" class="init">
     var species="<?php echo $species; ?>"; 
-    $(function(){
+    $(function () {
     $('#container').highcharts({
         chart: {
             type: 'column'
         },
         title: {
-            text: 'gene differential expression'
+            text: 'Column chart with negative values'
         },
         xAxis: {
-            categories: [species, 'Oranges', 'Pears', 'Grapes', 'Bananas']
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
         },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Total fruit consumption (Log2)'
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                }
-            }
-        },
-        legend: {
-            align: 'right',
-            x: -30,
-            verticalAlign: 'top',
-            y: 25,
-            floating: true,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-            borderColor: '#CCC',
-            borderWidth: 1,
-            shadow: false
-        },
-        tooltip: {
-            headerFormat: '<b>{point.x}</b><br/>',
-            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-        },
-        plotOptions: {
-            column: {
-                stacking: 'normal',
-                dataLabels: {
-                    enabled: true,
-                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
-                    style: {
-                        textShadow: '0 0 3px black'
-                    }
-                }
-            }
+        credits: {
+            enabled: false
         },
         series: [{
             name: 'John',
             data: [5, 3, 4, 7, 2]
         }, {
             name: 'Jane',
-            data: [2, 2, 3, 2, 1]
+            data: [2, -2, -3, 2, 1]
         }, {
             name: 'Joe',
-            data: [3, 4, 4, 2, 5]
+            data: [3, 4, 4, -2, 5]
         }]
     });
 });
