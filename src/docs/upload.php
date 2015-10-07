ca5b5d0e38d8e887fbb9df717d1025d34c11da19
benjamindartigues <bdartigues@gmail.com>
2015-10-07 17:13:58 +0200

    adding new features in measurements table

--- src/docs/upload.php
+++ src/docs/upload.php
@@ -26,7 +26,7 @@ $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"]["name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
