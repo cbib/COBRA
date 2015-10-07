c1467da82516d11026a540314e451663919d3a2d
benjamindartigues <bdartigues@gmail.com>
2015-10-07 13:07:55 +0200

    adding new features in measurements table

--- src/docs/index.php
+++ src/docs/index.php
@@ -1,8 +1,28 @@
-<?php
+<?php 
+include './functions/html_functions.php';
+include './functions/php_functions.php';
+include './functions/mongo_functions.php';
+include '../wiki/vendor/autoload.php';
+require('./session/control-session.php');
+$db=mongoConnector();
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//$grid = $db->getGridFS();
+////Selection des collections
+//$samplesCollection = new MongoCollection($db, "samples");
+//$speciesCollection = new Mongocollection($db, "species");
+//$mappingsCollection = new Mongocollection($db, "mappings");
+//$measurementsCollection = new Mongocollection($db, "measurements");
+//$virusesCollection = new Mongocollection($db, "viruses");
+//$interactionsCollection = new Mongocollection($db, "interactions");
+//$orthologsCollection = new Mongocollection($db, "orthologs");
+//$GOCollection = new Mongocollection($db, "gene_ontology");
+
+new_cobra_header();
+
+new_cobra_body($_SESSION['login'],"Upload files Page","section_upload_file");
+
+new_cobra_footer(); 
+  
+
+?>
 
