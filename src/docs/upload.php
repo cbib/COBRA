3fe74ecca7c50fe39229a9dc3e2ef3a62c9b10bb
benjamindartigues <bdartigues@gmail.com>
2015-10-07 15:38:21 +0200

    adding new features in measurements table

--- src/docs/upload.php
+++ src/docs/upload.php
@@ -28,7 +28,7 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";s
+        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
