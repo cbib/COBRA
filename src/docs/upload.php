e55988f0dd6ee98aeac78424708f8f673e8e5a21
benjamindartigues <bdartigues@gmail.com>
2015-10-14 10:31:30 +0200

    adding new features in measurements table

--- src/docs/upload.php
+++ src/docs/upload.php
@@ -44,7 +44,7 @@ if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
      $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
      if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
      {
-          echo 'Upload effectué avec succès !';
+          echo 'Your file '.$fichier.' was upload successfully !';
      }
      else //Sinon (la fonction renvoie FALSE).
      {
