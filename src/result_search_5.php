831404098558ad4cb91f0a0a4a9fba52e5aba015
benjamindartigues <bdartigues@gmail.com>
2015-10-14 15:04:01 +0200

    adding new features in measurements table

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -1576,12 +1576,13 @@ new_cobra_footer();
                     var g=genes;
                     //window.alert(genes);
                 
-                    s += '<ul><li>'+if (typeof variable === 'undefined' || variable === null) {}+'profile on Day '+ this.point.dpi +' post inoculation</li><li>Variety : '+ this.point.variety +'</li><li>logFC : '+ this.point.logFC +'</li></br>'
+                    s += '<ul><li>'+'profile on Day '+ this.point.dpi +' post inoculation</li><li>Variety : '+ this.point.variety +'</li><li>logFC : '+ this.point.logFC +'</li></br>'
                          '</ul>';
                    
                     return s;
                 }
             }
+            //if (typeof variable === 'undefined' || variable === null) {}
               
  
               //series: serie
