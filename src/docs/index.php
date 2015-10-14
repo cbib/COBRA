c386929669c227dafbabb9291ec38232283d7840
benjamindartigues <bdartigues@gmail.com>
2015-10-14 11:56:49 +0200

    adding new features in measurements table

--- src/docs/index.php
+++ src/docs/index.php
@@ -28,7 +28,8 @@ echo '<form action="upload.php" method="post" enctype="multipart/form-data">
         Select file to upload: <input type="file" name="fileToUpload" id="fileToUpload">
     
         <input type="submit" value="Upload File" name="submit">
-      </form>';
+      </form>
+      </div>';
 
 
 echo '<div id="section_documents">';
