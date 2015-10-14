c91ff32f76befad74c1f1dd1b336f89009aff15a
benjamindartigues <bdartigues@gmail.com>
2015-10-14 10:32:36 +0200

    adding new features in measurements table

--- src/docs/upload.php
+++ src/docs/upload.php
@@ -69,23 +69,23 @@ else
 
 
 
-$target_dir = "../COBRA_repository/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-//$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    
-    echo $_POST["submit"];
-//    $check = getimagesize($_FILES["fileToUpload"]["name"]);
-//    if($check !== false) {
-//        echo "File is an image - " . $check["mime"] . ".";
-//        $uploadOk = 1;
-//    } else {
-//        echo "File is not an image.";
-//        $uploadOk = 0;
-//    }
-}
+//$target_dir = "../COBRA_repository/";
+//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//$uploadOk = 1;
+////$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+//// Check if image file is a actual image or fake image
+//if(isset($_POST["submit"])) {
+//    
+//    echo $_POST["submit"];
+////    $check = getimagesize($_FILES["fileToUpload"]["name"]);
+////    if($check !== false) {
+////        echo "File is an image - " . $check["mime"] . ".";
+////        $uploadOk = 1;
+////    } else {
+////        echo "File is not an image.";
+////        $uploadOk = 0;
+////    }
+//}
 
 new_cobra_footer(); 
   
