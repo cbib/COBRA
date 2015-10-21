6122fb5400f3c585aaeebb3e0588c8d0a0de5c1a
benjamindartigues <bdartigues@gmail.com>
2015-10-21 16:36:39 +0200

    adding new features in measurements table

--- src/docs/index.php
+++ src/docs/index.php
@@ -79,8 +79,9 @@ if($dossier = opendir('./COBRA_depot/'))
                       <span class="sr-only">Toggle Dropdown</span>
                     </button>
                     <ul class="dropdown-menu">
-                      <li><a download="./COBRA_depot/'.$fichier.'">Download</a></li>
-                      <li><a href="./COBRA_depot/'.$fichier.'" download>Remove</a></li>
+                      
+                      <li><a href="./COBRA_depot/'.$fichier.'" download>Download file</a></li>
+                      <li><a href="#">Remove</a></li>
                       <!--<li role="separator" class="divider"></li>
                       <li><a href="#">Separated link</a></li>-->
                     </ul>
