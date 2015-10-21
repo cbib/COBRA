51a91b7e16dd72f9a0ad618656381c06952bd152
benjamindartigues <bdartigues@gmail.com>
2015-10-21 10:36:20 +0200

    adding new features in measurements table

--- src/docs/index.php
+++ src/docs/index.php
@@ -47,6 +47,7 @@ $table_string.='<thead><tr>';
 	//$table_string.='<th>type</th>';
 	$table_string.='<th>File name</th>';
 	$table_string.='<th>From</th>';
+    
 	
 
 	
@@ -70,6 +71,20 @@ if($dossier = opendir('./COBRA_depot/'))
             $table_string.='<td>'.$fichier.'</td>';
             $table_string.='<td>'.$_SESSION['firstname'].$_SESSION['lastname'].'</td>';
             //echo '<li><a href="./mondossier/' . $fichier . '">' . $fichier . '</a></li>';
+            $table_string.='<div class="btn-group">
+                    <button type="button" class="btn btn-danger">Action</button>
+                    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <span class="caret"></span>
+                      <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                    </ul>
+                  </div>';
             $table_string.='</tr>';
         }
        
