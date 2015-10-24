6b3edea237e3da71ba0a292019965aaffd62b85a
benjamindartigues <bdartigues@gmail.com>
2015-10-24 10:17:26 +0200

    improving datatable rendering

--- src/docs/index.php
+++ src/docs/index.php
@@ -195,7 +195,7 @@ $(document).ready(function() {
     
     $('#documents tbody').on( 'click', 'tr', function () {
         var $row = $(this);
-        var t1 = $row.first().text();
+        var t1 = $row.find('selected').text();
         //var t1 = $row.find(':nth-child(1)').text();
         alert(t1);
         if ( $(this).hasClass('selected') ) {
