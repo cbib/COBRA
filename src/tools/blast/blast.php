a49ae4d6ec0d665122d3cc96549a29aab95cc5db
benjamindartigues <bdartigues@gmail.com>
2015-11-24 10:51:58 +0100

    add script for insert Hordeum and Solanum sequences

--- src/tools/blast/blast.php
+++ src/tools/blast/blast.php
@@ -26,17 +26,17 @@ error_log('Here is the search id: '.$search_id.' and the sequence: '.$sequence);
 //".$blast_output." ".$job_name." ".$cfgBlastDbDir." ".$output_format. " 
 //".$options;
 //
-//$cmd = "/data/applications/ncbi/bin/blastx" .$cmd_string;
+$cmd = "/data/applications/ncbi-blast-2.2.31+/bin/blastx -query /data/applications/ncbi-blast-2.2.31+/tmp/test.fasta -db /data/applications/ncbi-blast-2.2.31+/db/cobra_blast_proteome_db -out /data/applications/ncbi-blast-2.2.31+/tmp/blast_results.txt -outfmt 13";// .$cmd_string;
+$output = exec($cmd);
 
 
-
-//./blastx -query ../tmp/test.fasta -db ../db/cobra_blast_proteome_db -out ../tmp/blast_results.txt -outfmt 13
+//./blastx -query /data/applications/ncbi-blast-2.2.31+/tmp/test.fasta -db /data/applications/ncbi-blast-2.2.31+/db/cobra_blast_proteome_db -out /data/applications/ncbi-blast-2.2.31+/tmp/blast_results.txt -outfmt 13
 
         
         
         /*  Execute command */
  
-//$output = exec($cmd);
+
 
 new_cobra_footer();	
 
