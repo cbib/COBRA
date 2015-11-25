e6132085bafcedcad3c8924eb746f9d438aaa509
benjamindartigues <bdartigues@gmail.com>
2015-11-25 15:24:45 +0100

    add script for insert Hordeum and Solanum sequences

---
+++ backend/tests/populate_with_full_mapping_table.py
@@ -0,0 +1,152 @@
+import sys
+sys.path.append("..")
+sys.path.append(".")
+from config import *
+from helpers.basics import load_config
+from helpers.logger import Logger
+from helpers.db_helpers import * 
+import string
+from random import *
+
+# Script 
+import datetime
+if "log" not in globals():
+  log = Logger.init_logger('SAMPLE_DATA_%s'%(cfg.language_code), load_config())
+
+# Clear collections to fill
+mappings_col.drop({"type":{"$in":["full_table"]}});
+
+#orthologs_col.drop()
+#interactions_col.drop()
+for grid_out in fs.find({}, timeout=False):
+	
+	fs.delete(grid_out._id)
+
+
+
+###################################################################################################################
+############################################ PRUNUS DOMESTICA #####################################################
+###################################################################################################################
+
+# full mapping table - PROBEID/GENEID/PROTEINID/DESCRIPTION/PLAZAID/ALIAS/GENEONTOLOGYID
+mapping_table={
+	"data_file":"mappings/prunus_full_table.tsv",
+	"species":"Prunus persica",
+	"type":"full_table",
+	"src":"PROBE_ID",
+	"src_version":"NCBI",
+	"url":"",
+	"doi":"none",
+	"key":"PROBEID/GENEID/GENEIDBIS/PROTEINIDALT1/PROTEINIDALT2/UNIPROTID/DESCRIPTION/PLAZAID/GENEONTOLOGYID",
+	# parser config 
+		# xls parser configuration, are propagated to all entries in  "experimental_results",
+	"xls_parsing":{
+		"n_rows_to_skip":0,
+		"column_keys":['idx','Gene ID','Gene ID 2','Protein ID','Protein ID 2','Uniprot ID','Description','Plaza ID','Gene ontology ID'],
+		"sheet_index":0,
+	}
+}
+mappings_col.insert(mapping_table)
+
+###################################################################################################################
+############################################ CUCUMIS MELO #########################################################
+###################################################################################################################
+
+# full mapping table - PROBEID/GENEID/PROTEINID/DESCRIPTION/PLAZAID/ALIAS/GENEONTOLOGYID
+mapping_table={
+	"data_file":"mappings/cucumis_melo_full.tsv",
+	"species":"Cucumis melo",
+	"type":"full_table",
+	"src":"PROBE_ID",
+	"src_version":"est ICUGI v4",
+        "tgt":"multiple",
+        "tgt_version":"multiple",
+	"url":"",
+	"doi":"none",
+	"key":"PROBEID/GENEID/GENEIDBIS/PROTEINID/DESCRIPTION/PLAZAID/GENEONTOLOGYID",
+	# parser config 
+		# xls parser configuration, are propagated to all entries in  "experimental_results",
+	"xls_parsing":{
+		"n_rows_to_skip":1,
+		"column_keys":['idx','Probe ID','Gene ID','Gene ID 2','Uniprot ID','Description','Plaza ID','Gene ontology ID'],
+		"sheet_index":0,
+	}
+}
+mappings_col.insert(mapping_table)
+
+
+###################################################################################################################
+############################################ HORDEUM VULGARE ######################################################
+###################################################################################################################
+
+# full mapping table - PROBEID/GENEID/PROTEINID/DESCRIPTION/PLAZAID/ALIAS/GENEONTOLOGYID
+mapping_table={
+	"data_file":"mappings/hordeum_vulgare_full.tsv",
+	"species":"Hordeum vulgare",
+	"type":"full_table",
+	"src":"PROBE_ID",
+	"src_version":"Morex contig",
+        "tgt":"Multiple",
+        "tgt_version":"Multiple target",
+	"url":"",
+	"doi":"none",
+	"key":"PROBEID/GENEID/GENEIDBIS/PROTEINID/DESCRIPTION/PLAZAID/GENEONTOLOGYID",
+	# parser config 
+		# xls parser configuration, are propagated to all entries in  "experimental_results",
+	"xls_parsing":{
+		"n_rows_to_skip":1,
+		"column_keys":['idx','Probe ID','Gene ID','Transcript ID','Uniprot ID','Description','Gene name','Plaza ID','Gene ontology ID'],
+		"sheet_index":0,
+	}
+}
+mappings_col.insert(mapping_table)
+
+###################################################################################################################
+############################################ SOLANUM LYCOPERSICUM #################################################
+###################################################################################################################
+
+
+# full mapping table - PROBEID/GENEID/PROTEINID/DESCRIPTION/PLAZAID/ALIAS/GENEONTOLOGYID
+mapping_table={
+	"data_file":"mappings/solanum_lycopersicum_full.tsv",
+	"species":"Solanum lycopersicum",
+	"type":"full_table",
+	"src":"PROBE_ID",
+	"src_version":"SolGenomics",
+	"url":"",
+	"doi":"none",
+	"key":"PROBEID/GENEID/TRANSCRIPTID/PROTEINID/ALIAS/DESCRIPTION/PLAZAID/GENEONTOLOGYID",
+	# parser config 
+		# xls parser configuration, are propagated to all entries in  "experimental_results",
+	"xls_parsing":{
+		"n_rows_to_skip":0,
+		"column_keys":['idx','Probe ID','Gene ID','Transcript ID','Uniprot ID','Alias','Description','Plaza ID','Gene ontology ID'],
+		"sheet_index":0,
+	}
+}
+mappings_col.insert(mapping_table)
+
+
+###################################################################################################################
+############################################ ARABIDOPSIS THALIANA #################################################
+###################################################################################################################
+
+# full mapping table - PROBEID/GENEID/PROTEINID/DESCRIPTION/PLAZAID/ALIAS/GENEONTOLOGYID
+mapping_table={
+	"data_file":"mappings/arabidopsis_thaliana_full.tsv",
+	"species":"Arabidopsis thaliana",
+	"type":"full_table",
+	"src":"CATMA_ID",
+	"src_version":"CATMA V2.1",
+	"url":"ftp://urgv.evry.inra.fr/CATdb/array_design/CATMA_2.3_07122011.txt",
+	"doi":"none",
+	"key":"PROBEID/GENEID/GENEIDBIS/PROTEINID/DESCRIPTION/PLAZAID/ALIAS/GENEONTOLOGYID",
+	# parser config 
+		# xls parser configuration, are propagated to all entries in  "experimental_results",
+	"xls_parsing":{
+		"n_rows_to_skip":1,
+		"column_keys":['idx','Probe ID','Gene ID','Gene ID 2','Uniprot ID','Description','Plaza ID','Alias','Gene ontology ID','Symbol'],
+		"sheet_index":0,
+	}
+}
+mappings_col.insert(mapping_table)
