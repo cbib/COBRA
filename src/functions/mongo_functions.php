dfd11783aefce98c029ca5fc896948eb0185930a
benjamindartigues <bdartigues@gmail.com>
2015-11-26 17:44:26 +0100

    add script for insert Hordeum and Solanum sequences

--- src/functions/mongo_functions.php
+++ src/functions/mongo_functions.php
@@ -917,7 +917,8 @@ function count_transcript_for_gene(Mongocollection $sequencesCollection,$gene_id
     foreach ($sequences_cursor as $result) {
         foreach ($result as $data){
             
-            var_dump($data['mapping_file']['Transcript ID']);
+            //var_dump($data['mapping_file']['Transcript ID']);
+            array_push($transcript_count, $data['mapping_file']['Transcript ID']);
         }
         
 //        var_dump($result['mapping_file']);
