4b3d8d40d247042e666c2bd46c667fce5235524e
benjamindartigues <bdartigues@gmail.com>
2015-11-27 17:15:30 +0100

    add script for insert Hordeum and Solanum sequences

--- src/tools/blast/blast.php
+++ src/tools/blast/blast.php
@@ -53,10 +53,10 @@ if ((isset($_POST['search'])) && ($_POST['search']!='')){
     }
     //it works below
     //$output = shell_exec('/data/applications/ncbi-blast-2.2.31+/bin/blastx -query /data/applications/ncbi-blast-2.2.31+/tmp/test.fasta -db /data/applications/ncbi-blast-2.2.31+/db/cobra_blast_proteome_db -out /data/applications/ncbi-blast-2.2.31+/tmp/blast_results4.txt -outfmt 13');
-    $tmp=substr(str_shuffle(MD5(microtime())), 0, 10);
+    $tmp=substr(str_shuffle(MD5(microtime())), 0, 20);
     //error_log($tmp) ;
-    $query_file="/data/applications/ncbi-blast-2.2.31+/tmp/$tmp'_'$search_id'.fasta";
-    $result_file = "/data/applications/ncbi-blast-2.2.31+/tmp/$tmp'_blast_results.txt";
+    $query_file="/data/applications/ncbi-blast-2.2.31+/tmp/$tmp.fasta";
+    $result_file = "/data/applications/ncbi-blast-2.2.31+/tmp/$tmp.txt";
     //$output = shell_exec('/data/applications/ncbi-blast-2.2.31+/bin/blastx -query /data/applications/ncbi-blast-2.2.31+/tmp/'.$tmp.'_'.$search_id.'.fasta -db /data/applications/ncbi-blast-2.2.31+/db/cobra_blast_proteome_db -out /data/applications/ncbi-blast-2.2.31+/tmp/'.$tmp.'_blast_results.txt -outfmt 13');
     $output = shell_exec("/data/applications/ncbi-blast-2.2.31+/bin/blastx -query $query_file -db /data/applications/ncbi-blast-2.2.31+/db/cobra_blast_proteome_db -out $result_file -outfmt 13");
      //sprintf('/data/applications/ncbi-blast-2.2.31+/bin/blastx -query %s -db /data/applications/ncbi-blast-2.2.31+/db/cobra_blast_proteome_db -out %s -outfmt 13',$query_file,$result_file);
