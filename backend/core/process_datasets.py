f2913adbd31b4304bbafefffdeceaeb44edacfcb
benjamindartigues <bdartigues@gmail.com>
2015-12-04 15:23:39 +0100

    adding new sample for tomato

--- backend/core/process_datasets.py
+++ backend/core/process_datasets.py
@@ -41,7 +41,7 @@ for a_sample in samples_to_process:
                 src_file= data_dir+a_result['data_file']
                 fileName, fileExtension = os.path.splitext(src_file)
                 if fileExtension!='.xls' and fileExtension!='.xlsx':
-                    parsed_data = parse_tsv_table(src_file,parser_config['column_keys'],parser_config['n_rows_to_skip'],parser_config['sheet_index'],parser_config['id_type'])
+                    parsed_data = parse_tsv_table(src_file,parser_config['column_keys'],parser_config['n_rows_to_skip'],parser_config['id_type'])
 
                 else:
                     parsed_data = parse_excel_table(src_file,parser_config['column_keys'],parser_config['n_rows_to_skip'],parser_config['sheet_index'],parser_config['id_type'])
