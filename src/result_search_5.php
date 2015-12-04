80fb09496e7c749fad3fb0bac2fee5e114c0beff
benjamindartigues <bdartigues@gmail.com>
2015-12-04 10:27:08 +0100

    modify li header in interaction ul

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -188,7 +188,7 @@ echo   '<div id="summary">';
                         $xp_full_name=explode(".", $result['xp']);                   
                         $experiment_id=$xp_full_name[0];
                         $xp_name=explode(".", get_experiment_name_with_id($samplesCollection,$experiment_id));
-                        echo $xp_name.'and'.$result['logFC'];
+                        echo $result['logFC'].'</br>';
                         if (isset($result['day_after_inoculation'])){
                             if (isset($result['variety'])){
                                $sample=array('y'=>$result['logFC'],'dpi'=>$result['day_after_inoculation'],'variety'=>$result['variety'],'logFC'=>$result['logFC']);
