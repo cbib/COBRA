9f392108bf7751830f82d6a70c859f9cf07a784d
benjamindartigues <bdartigues@gmail.com>
2015-12-23 08:37:12 +0100

    modifying interaction biogrid request

--- src/session/control-session.php
+++ src/session/control-session.php
@@ -6,12 +6,13 @@ session_start(); // ici on continue la session
  
 if ((!isset($_SESSION['login'])) || ($_SESSION['login'] === ''))
 {
-    error_log("into control session and in directory ". MAINDIR);
+    
     //header('Location: /var/www/html/COBRA/login.php'); 
     
     $host  = $_SERVER['HTTP_HOST'];
     $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
     $extra = 'login.php';
+    error_log("into control session and in directory ". $host ."/".$uri."/".$extra);
     header("Location: http://$host$uri/$extra");
     //http://127.0.0.1/Users/benjamindartigues/COBRA/GIT/COBRA/login.php
 //    new_cobra_header();
