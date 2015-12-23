9833051e45c6924c8f5002f6ad9144b2cc2f30af
benjamindartigues <bdartigues@gmail.com>
2015-12-23 12:40:08 +0100

    modifying interaction biogrid request

--- src/session/maintenance-session.php
+++ src/session/maintenance-session.php
@@ -5,12 +5,24 @@
  
 if ((isset($_SESSION['maintenance'])) && ($_SESSION['maintenance'] == 'yes') && (($_SESSION['firstname'] != 'Dartigues') && ($_SESSION['firstname'] != 'Benaben')))
 {
-    header('Location: ../../maintenance.php'); 
+    //header('Location: ../../maintenance.php'); 
 //    new_cobra_header();
 //    new_cobra_body(False, "Login form");
 //	echo '<p>You have to be <a href="/login.php"> logged</a>.</p>'."\n";
 //	new_cobra_footer();
 //    exit();
+    $host  = $_SERVER['HTTP_HOST'];
+    //$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $uri="cobra";
+    $extra = 'maintenance.php';
+    if ($host==="127.0.0.1"){
+        //error_log("into control session and in directory ". $host ."/".$extra);
+        header("Location: http://$host/$extra"); 
+    }
+    else{
+        //error_log("into control session and in directory ". $host ."/".$uri."/".$extra);
+        header("Location: http://$host/$uri/$extra"); 
+    }
 }
  
 ?>
