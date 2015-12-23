c07fc4952c897dda6fec66a54309a447659e0faf
benjamindartigues <bdartigues@gmail.com>
2015-12-23 08:19:05 +0100

    modifying interaction biogrid request

--- src/session/control-session.php
+++ src/session/control-session.php
@@ -7,7 +7,7 @@ session_start(); // ici on continue la session
 if ((!isset($_SESSION['login'])) || ($_SESSION['login'] === ''))
 {
     error_log("into control session and in directory".$_SERVER['DOCUMENT_ROOT']);
-    header('Location: ../../login.php'); 
+    header('Location: /login.php'); 
 //    new_cobra_header();
 //    new_cobra_body(False, "Login form");
 //	echo '<p>You have to be <a href="/login.php"> logged</a>.</p>'."\n";
