ca3d7c0f4ef74addfd7c484d0adb74d2a30dff57
benjamindartigues <bdartigues@gmail.com>
2016-01-05 18:53:31 +0100

    modifying interaction biogrid request

--- src/functions/php_functions.php
+++ src/functions/php_functions.php
@@ -577,7 +577,7 @@ function display_expression_profile(MongoCollection $measurementsCollection, Mon
 function display_proteins_details(array $gene_id, array $gene_symbol, array $gene_alias, array $descriptions, array $proteins_id,$species='null',$score='null'){
    echo'<div id="section_description"><B>'.$gene_id[0].'</B> ';for ($i = 0; $i < $score; $i++) { 
                    echo '<i class="fa fa-star"></i>';
-                };echo'<B class="right">'.$species.'</B>
+                };echo'<B class="right"><i>'.$species.'</i></B>
                     ';
                 echo '<h1>';
                 
