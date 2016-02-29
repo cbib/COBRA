3ff406a6cf9f8ebcdad2baa4a5706a1988100602
benjamindartigues <bdartigues@gmail.com>
2016-02-29 16:50:26 +0100

    add interaction scoring part + ortholog scoring part

--- backend/core/process_interaction_scoring.py
+++ backend/core/process_interaction_scoring.py
@@ -40,7 +40,7 @@ for species in species_to_process:
                         split_list=r['Gene ID'].split('-')
                         for gene_id in split_list:
                             logger.info("gene id %s ",gene_id)
-                            full_mappings_col.update({'mapping_file.Gene ID':gene_id},{'$inc': {'mapping_file.$.Score': 1 } })
+                            full_mappings_col.update({'mapping_file.Gene ID':gene_id},{'$inc': {'mapping_file.$.Score_int': 1 } })
                             plaza_results=full_mappings_col.find({'mapping_file.Gene ID':gene_id},{'mapping_file.$': 1 } )
                             for p in plaza_results:
                                 for values in p['mapping_file']:
@@ -59,16 +59,16 @@ for species in species_to_process:
                                                 if ortholog_id!=plaza_id:
                                                     logger.info("ortholog id %s ",ortholog_id)
 
-                                                    full_mappings_col.update({"mapping_file.Plaza ID":ortholog_id},{"$inc": {'mapping_file.$.Score': 1 } })
+                                                    full_mappings_col.update({"mapping_file.Plaza ID":ortholog_id},{"$inc": {'mapping_file.$.Score_orthologs': 1 } })
                                         else:
                                             if ortholog_list!=plaza_id:
                                                 logger.info("ortholog id %s ",ortholog_list)
 
-                                                full_mappings_col.update({"mapping_file.Plaza ID":ortholog_list},{"$inc": {'mapping_file.$.Score': 1 } })
+                                                full_mappings_col.update({"mapping_file.Plaza ID":ortholog_list},{"$inc": {'mapping_file.$.Score_orthologs': 1 } })
 
                     else:
                         logger.info("gene id %s ",r['Gene ID'])
-                        full_mappings_col.update({'mapping_file.Gene ID':r['Gene ID']},{'$inc': {'mapping_file.$.Score': 1 } })
+                        full_mappings_col.update({'mapping_file.Gene ID':r['Gene ID']},{'$inc': {'mapping_file.$.Score_int': 1 } })
                         plaza_results=full_mappings_col.find({'mapping_file.Gene ID':r['Gene ID']},{'mapping_file.$': 1 } )
                         for p in plaza_results:
                             for values in p['mapping_file']:
@@ -86,12 +86,12 @@ for species in species_to_process:
                                             if ortholog_id!=plaza_id:
                                                 logger.info("ortholog id %s ",ortholog_id)
 
-                                                full_mappings_col.update({'mapping_file.Plaza ID':ortholog_id},{'$inc': {'mapping_file.$.Score': 1 } })
+                                                full_mappings_col.update({'mapping_file.Plaza ID':ortholog_id},{'$inc': {'mapping_file.$.Score_orthologs': 0.5 } })
                                     else:
                                         if ortholog_list!=plaza_id:
                                             logger.info("ortholog id %s ",ortholog_list)
 
-                                            full_mappings_col.update({'mapping_file.Plaza ID':ortholog_list},{'$inc': {'mapping_file.$.Score': 1 } })
+                                            full_mappings_col.update({'mapping_file.Plaza ID':ortholog_list},{'$inc': {'mapping_file.$.Score_orthologs': 0.5 } })
 
 
     #score all genes in hpidb interaction tables !!!
@@ -102,7 +102,7 @@ for species in species_to_process:
             if 'Uniprot ID' in r:
                 if r['Uniprot ID']!="":
                     logger.info("uniprot id %s ",r['Uniprot ID'])
-                    full_mappings_col.update({'mapping_file.Uniprot ID':r['Uniprot ID']},{'$inc': {'mapping_file.$.Score': 1 } })
+                    full_mappings_col.update({'mapping_file.Uniprot ID':r['Uniprot ID']},{'$inc': {'mapping_file.$.Score_int': 1 } })
 
                     plaza_results=full_mappings_col.find({'mapping_file.Uniprot ID':r['Uniprot ID']},{'mapping_file.$.Plaza ID': 1 } )
                     for p in plaza_results:
@@ -121,8 +121,8 @@ for species in species_to_process:
                                         for ortholog_id in ortholog_split_list:
                                             if ortholog_id!=plaza_id:
                                                 logger.info("ortholog id %s ",ortholog_id)
-                                                full_mappings_col.update({'mapping_file.Plaza ID':ortholog_id},{'$inc': {'mapping_file.$.Score': 1 } })
+                                                full_mappings_col.update({'mapping_file.Plaza ID':ortholog_id},{'$inc': {'mapping_file.$.Score_orthologs': 0.5 } })
                                     else:
                                         if ortholog_list!=plaza_id:
                                             logger.info("ortholog id %s ",ortholog_list)
-                                            full_mappings_col.update({'mapping_file.Plaza ID':ortholog_list},{'$inc': {'mapping_file.$.Score': 1 } })
+                                            full_mappings_col.update({'mapping_file.Plaza ID':ortholog_list},{'$inc': {'mapping_file.$.Score_orthologs': 0.5 } })
