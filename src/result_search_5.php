2fbfefe431f9aae57ebe43d652a0e277561c5795
benjamindartigues <bdartigues@gmail.com>
2016-02-29 18:30:45 +0100

    add interaction scoring part + ortholog scoring part

--- src/result_search_5.php
+++ src/result_search_5.php
@@ -74,7 +74,9 @@ if ((isset($_GET['organism'])  && $_GET['organism']!='' && $_GET['organism']!='N
            '$group'=>
              array(
                '_id'=> array( 'gene'=> '$mapping_file.Gene ID' ),
-               'scores'=> array('$addToSet'=> array('$mapping_file.Score_exp','$mapping_file.Score_int','$mapping_file.Score_orthologs','$mapping_file.Score_QTL','$mapping_file.Score_SNP') )
+               'scores'=> array('$addToSet'=> '$mapping_file.Score_exp')
+
+               //'scores'=> array('$addToSet'=> array('$mapping_file.Score_exp','$mapping_file.Score_int','$mapping_file.Score_orthologs','$mapping_file.Score_QTL','$mapping_file.Score_SNP') )
              )
          )
 
