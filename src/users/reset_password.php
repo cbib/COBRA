4772c3990b22b9cec4b8e818bd28822baf0a5cfd
benjamindartigues <bdartigues@gmail.com>
2016-05-19 16:23:55 +0200

    changing search page text

--- src/users/reset_password.php
+++ src/users/reset_password.php
@@ -16,17 +16,25 @@
 		
 		//here update new password using md5 command
 		if ($_GET['pwd1']==$_GET['pwd2']){
-            if (check_password($_GET['pwd1'])!=""){
-                $error=check_password($_GET['pwd1']);
+            $error=check_password($_GET['pwd1']);
+            if ($error!=""){
+                
+                new_cobra_body($_SESSION['login'],"Password Error","section_password_failure","../..");
+                
                 $error_message="Password validation failure(your choise is weak): $error";
-                echo "<script>alert($error_message);</script>"; 
+                //echo "<script>alert($error_message);</script>"; 
+                echo "<p> $error_message</p>";
+                echo '<a href=""></a>';
+                $firstname=$_SESSION['firstname'];
+                $lastname=$_SESSION['lastname'];
+                echo '<p>Back to Cobra?  <a href="./user.php?firstname='.$firstname.'&lastname='.$lastname.'">Click here</a></p>'."\n";
                 //header ('Location: ../search/index.php');
             }
             else{
                 new_cobra_body($_SESSION['login'],"Password Details","section_reset_password","../..");
                 $pwd=md5($_GET['pwd1']);
                 $usersCollection->update(array('firstname'=>$_SESSION['firstname'],'lastname'=>$_SESSION['lastname']), array('$set' => array('pwd' => $pwd)));
-                echo "<p> your new password has been successfully saved";
+                echo "<p> your new password has been successfully saved </p>";
                 header ('Location: ../search/index.php');
                 
             }
