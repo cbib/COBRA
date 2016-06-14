c5b7576065957e52fa649f9fc2fab8ad92641c7e
benjamindartigues <bdartigues@gmail.com>
2016-06-14 15:11:46 +0200

    adding data for rice japonica

--- src/functions/mongo_functions.php
+++ src/functions/mongo_functions.php
@@ -1184,8 +1184,9 @@ function get_ortholog_table(MongoCollection $full_mappingsCollection, Mongocolle
     if ($current_plaza_id!=""){
         //$timestart=microtime(true);
         $cursors=$orthologsCollection->find(array('mapping_file.Plaza gene id'=>$current_plaza_id),array('mapping_file.$'=>1,'_id'=>0));
-        var_dump($cursors);
+       
         foreach ($cursors as $cursor){
+             var_dump($cursor);
             foreach ($cursor as $mapping_file){
                 foreach ($mapping_file as $value){
                     //$ortholog_list_id=$value['orthologs_list_identifier'];
