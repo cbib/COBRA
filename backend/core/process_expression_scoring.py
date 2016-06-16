d9787650036bc74d5ed6d6c250ae2b3cf1712078
benjamindartigues <bdartigues@gmail.com>
2016-06-16 16:55:21 +0200

    adding data for rice japonica

--- backend/core/process_expression_scoring.py
+++ backend/core/process_expression_scoring.py
@@ -110,15 +110,15 @@ for species in species_to_process:
                   
                 plaza_results=full_mappings_col.find({'species':"Arabidopsis thaliana",'mapping_file.Gene ID':r['gene']},{'mapping_file.$.Plaza ID': 1 } )
                 
-            elif species['full_name']== "Arabidopsis thaliana":
+            elif species['full_name']== "Oriza sativa ssp japonica":
                 #logger.info("gene id %s for probe id %s",r['gene'],r['gene_original_id'])
                 if 'gene_original_id' in r:
 
-                    full_mappings_col.update({'species':"Oriza sativa ssp japonica",'mapping_file.Gene ID':r['gene'],'mapping_file.Probe ID':r['gene_original_id']},{'$inc': {'mapping_file.$.Score_exp': 1,'mapping_file.$.Global_Score': 1 } })
+                    full_mappings_col.update({'species':species["full_name"],'mapping_file.Gene ID':r['gene'],'mapping_file.Probe ID':r['gene_original_id']},{'$inc': {'mapping_file.$.Score_exp': 1,'mapping_file.$.Global_Score': 1 } })
                 else:
-                    full_mappings_col.update({'species':"Oriza sativa ssp japonica",'mapping_file.Gene ID':r['gene']},{'$inc': {'mapping_file.$.Score_exp': 1,'mapping_file.$.Global_Score': 1 } })
+                    full_mappings_col.update({'species':species["full_name"],'mapping_file.Gene ID':r['gene']},{'$inc': {'mapping_file.$.Score_exp': 1,'mapping_file.$.Global_Score': 1 } })
                   
-                plaza_results=full_mappings_col.find({'species':"Oriza sativa ssp japonica",'mapping_file.Gene ID':r['gene']},{'mapping_file.$.Plaza ID': 1 } )
+                plaza_results=full_mappings_col.find({'species':species["full_name"],'mapping_file.Gene ID':r['gene']},{'mapping_file.$.Plaza ID': 1 } )
 
             else:
                 if 'gene_original_id' in r:
