47c726c0eb776b018719ef0ba3a55d63a151d710
benjamindartigues <bdartigues@gmail.com>
2016-06-17 21:22:32 +0200

    adding data for rice japonica

--- backend/tests/populate_with_full_mapping_table.py
+++ backend/tests/populate_with_full_mapping_table.py
@@ -327,7 +327,7 @@ full_mappings_col.insert(mapping_table)
 
 mapping_table={
 	"data_file":"mappings/FULL_MAPPING/full_oriza_sativa_final1.tsv",
-	"species":"Oriza Sativa ssp. Japonica",
+	"species":"Oriza sativa ssp japonica",
 	"type":"full_table",
 	"src":"Gene ID",
 	"src_version":"IRGSP-1.0",
