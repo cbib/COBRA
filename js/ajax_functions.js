2bdd8c113c5a9b92c92f839fcaa8ffd7def10161
benjamindartigues <bdartigues@gmail.com>
2016-06-29 11:06:49 +0200

    adding GO enrichment tool  for expression profiles

--- js/ajax_functions.js
+++ js/ajax_functions.js
@@ -1241,7 +1241,34 @@ $(document).ready(function() {
         	}
 	});
 });
-
+$(document).ready(function() {
+	$('#S-genes').dataTable( {
+		"scrollX": true,
+		"jQueryUI": true,
+		"pagingType": "full_numbers",
+		"oLanguage": { 
+			"sProcessing":   "Processing...",
+			"sLengthMenu":   "display _MENU_ items",
+			"sZeroRecords":  "No item found",
+			"sInfo": "Showing item _START_ to _END_ on  _TOTAL_ items",
+			"sInfoEmpty": "Displaying item 0 to 0 on 0 items",
+			"sInfoFiltered": "(filtered from _MAX_ items in total)",
+			"sInfoPostFix":  "",
+			"sSearch":       "Search: ",
+			"sUrl":          "",
+			"oPaginate": {
+				"sFirst":    "First",
+				"sPrevious": "Previous",
+				"sNext":     "Next",
+				"sLast":     "Last"
+			}
+		},
+		"language": {
+            		"decimal": ",",
+            		"thousands": "."
+        	}
+	});
+});
 
 
 
