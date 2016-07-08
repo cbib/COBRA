ddb43120d973905354e7c8d90a975a2990b8f8b2
benjamindartigues <bdartigues@gmail.com>
2016-07-08 14:43:33 +0200

    adding GO enrichment tool  for expression profiles

--- backend/core/process_kegg_pathway.py
+++ backend/core/process_kegg_pathway.py
@@ -23,61 +23,219 @@ if "log" not in globals():
 import urllib2
 
 
+species_id=sys.argv[1]
+species_name=sys.argv[2]
+species_name=species_name.replace("_", " ")
+
+
 classe="Genetic Information Processing"
 type="Transcription"
-type_dict_transcription={'RNA polymerase':'03020','Basal transcription factors':'03022','Spliceosome':'03040'}
+type_dict={'RNA polymerase':'03020','Basal transcription factors':'03022','Spliceosome':'03040'}
 
-for key, value in type_dict_transcription.items():
+for key, value in type_dict.items():
     
-    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/pper"+value).read()
-    print my_pathway
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    print key
+    gene_list=[]
     for path in my_pathway.split(	):
-	print path  
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
     
     
-#    kegg_pathway_table={
-#
-#        'species':'Prunus persica',
-#        'src':'Kegg_pathway',
-#        'tgt':'gene_list',
-#        'type':type,
-#        'data':
-#
-#    }
-#    kegg_pathway_col.insert(orthologs_table)
-#type="Translation"
-#type_dict_translation={ 'Ribosome':'03010','Aminoacyl-tRNA biosynthesis':'00970','RNA transport':'03013','mRNA surveillance pathway':'03015','Ribosome biogenesis in eukaryotes':'03008'}
-#
-#kegg_pathway_table={
-#
-#    'species':'Prunus persica',
-#    'src':'Kegg_pathway',
-#    'tgt':'gene_list'
-#
-#}
-#kegg_pathway_col.insert(orthologs_table)
-#
-#type="Folding, sorting and degradation"
-#type_dict_folding={ 'Protein export':'03060','Protein processing in endoplasmic reticulum':'04141','SNARE interactions in vesicular transport':'04130','Ubiquitin mediated proteolysis':'04120','Sulfur relay system':'04122','Proteasome':'03050','RNA degradation':'03018'}
-#type="Replication and repair"
-#type_dict_replication={ 'DNA replication':'03030','Base excision repair':'03410','Nucleotide excision repair':'03420','Mismatch repair':'03430','Homologous recombination':'03440','Non-homologous end-joining':'03450'}
-#
-#
-#   
-#
-#classe2="Environmental Information Processing"
-#type="Membrane transport"
-#type_dict_transport_membrane={'ABC transporters':'02010'}
-#
-#type="Signal transduction"
-#type_dict_signal_transduction={'Phosphatidylinositol signaling system':'04070','Plant hormone signal transduction':'04075'}
-#
-#
-#classe3="Cellular Processes"
-#
-#type="Transport and catabolism"
-#type_dict_transandcat={'Endocytosis':'04144','Phagosome':'04145','Peroxisome':'04146','Regulation of autophagy':'04140'}
-#
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
+type="Translation"
+type_dict={ 'Ribosome':'03010','Aminoacyl-tRNA biosynthesis':'00970','RNA transport':'03013','mRNA surveillance pathway':'03015','Ribosome biogenesis in eukaryotes':'03008'}
+
+for key, value in type_dict.items():
+    
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    print key
+    gene_list=[]
+    for path in my_pathway.split(	):
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
+    
+    
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
+
+
+
+type="Folding, sorting and degradation"
+type_dict={ 'Protein export':'03060','Protein processing in endoplasmic reticulum':'04141','SNARE interactions in vesicular transport':'04130','Ubiquitin mediated proteolysis':'04120','Sulfur relay system':'04122','Proteasome':'03050','RNA degradation':'03018'}
+
+for key, value in type_dict.items():
+    
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    
+    gene_list=[]
+    for path in my_pathway.split(	):
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
+    
+    
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
+
+
+type="Replication and repair"
+type_dict={ 'DNA replication':'03030','Base excision repair':'03410','Nucleotide excision repair':'03420','Mismatch repair':'03430','Homologous recombination':'03440','Non-homologous end-joining':'03450'}
+
+for key, value in type_dict.items():
+    
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    print key
+    gene_list=[]
+    for path in my_pathway.split(	):
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
+    
+    
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
+   
+
+classe2="Environmental Information Processing"
+type="Membrane transport"
+type_dict={'ABC transporters':'02010'}
+for key, value in type_dict.items():
+    
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    print key
+    gene_list=[]
+    for path in my_pathway.split(	):
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
+    
+    
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe2,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
+
+
+
+type="Signal transduction"
+type_dict={'Phosphatidylinositol signaling system':'04070','Plant hormone signal transduction':'04075'}
+for key, value in type_dict.items():
+    
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    print key
+    gene_list=[]
+    for path in my_pathway.split(	):
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
+    
+    
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe2,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
+
+classe3="Cellular Processes"
+
+type="Transport and catabolism"
+type_dict={'Endocytosis':'04144','Phagosome':'04145','Peroxisome':'04146','Regulation of autophagy':'04140'}
+for key, value in type_dict.items():
+    
+    my_pathway=urllib2.urlopen("http://rest.kegg.jp/link/genes/"+species_id+value).read()
+    print key
+    gene_list=[]
+    for path in my_pathway.split(	):
+        if "path" not in path: 
+            gene_id= path.split(":")[1] 
+            if "PRUPEppa" in  gene_id :
+                print "PRUPEppa found in "+ gene_id
+                #gene_id.replace("PRUPEppa", "PRUPE_ppa")
+                gene_list.append(gene_id.replace("PRUPEppa", "PRUPE_ppa"))
+            else:
+                gene_list.append(gene_id)
+    
+    
+    kegg_pathway_table={
+
+        'species':species_name,
+        'class':classe3,
+        'type':type,
+        'data':gene_list
+
+    }
+    kegg_pathway_col.insert(kegg_pathway_table)
 #
 #Organismal Systems
 #
