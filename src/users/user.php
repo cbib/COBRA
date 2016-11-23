8d5ef9910bdb4d28f46fe8532ebe453aac248c6c
ben <ben@brainben>
2016-11-23 11:51:05 +0100

    add no results information for expression profile

--- src/users/user.php
+++ src/users/user.php
@@ -1,34 +1,34 @@
  <?php
- 	//session_start();
- 	include '../functions/html_functions.php';
- 	include '../functions/php_functions.php';
- 	include '../functions/mongo_functions.php';
- 	require '../session/control-session.php';
- 
-	new_cobra_header("../..");
+    //session_start();
+    include '../functions/html_functions.php';
+    include '../functions/php_functions.php';
+    include '../functions/mongo_functions.php';
+    require '../session/control-session.php';
+
+    new_cobra_header("../..");
 
 
-	new_cobra_body($_SESSION['login'],"Users details","section_user_details","../..");
- 	if ((isset($_GET['firstname'])) && ((isset($_GET['lastname'])))){
- 		$firstname=htmlentities(trim($_GET['firstname']));
- 		$lastname=htmlentities(trim($_GET['lastname']));
- 	
- 	}
- 	else{
- 		echo '<p>You are not registered </p>'."\n";
- 		exit();
+    new_cobra_body($_SESSION['login'],"Users details","section_user_details","../..");
+    if ((isset($_GET['firstname'])) && ((isset($_GET['lastname'])))){
+            $firstname=htmlentities(trim($_GET['firstname']));
+            $lastname=htmlentities(trim($_GET['lastname']));
 
- 	}
- 	$db=mongoConnector();
-	$usersCollection = new Mongocollection($db, "users");
+    }
+    else{
+            echo '<p>You are not registered </p>'."\n";
+            exit();
+
+    }
+    $db=mongoConnector();
+    $usersCollection = new Mongocollection($db, "users");
     $jobsCollection = new Mongocollection($db, "jobs");
     $speciesCollection = new Mongocollection($db, "species");
     $historyCollection = new Mongocollection($db, "history");
     $GO_enrichedCollection = new Mongocollection($db, "go_enrichments");
 
-	$user=$usersCollection->find(array("firstname"=>$firstname,"lastname"=>$lastname),array());
+    $user=$usersCollection->find(array("firstname"=>str($firstname),"lastname"=>str($lastname)),array());
     make_species_list(find_species_list($speciesCollection),"../..");
- 	make_user_preferences($user,$usersCollection);
+    make_user_preferences($user,$usersCollection);
     
     //une table avec tous les GO enrichments
     
