dbf2d52f86d20358a000f2f310dcf9cad81284f4
benjamindartigues <bdartigues@gmail.com>
2016-11-23 18:23:22 +0100

    changing COBRA doc repository

--- src/docs/index.php
+++ src/docs/index.php
@@ -218,7 +218,7 @@ echo '<div id="doc_pages">';
                     <ul class="dropdown-menu">
 
                       <li><a href="./'.$dir.$fichier.'">Download file</a></li>
-                      <li><a href="./'.$dir.$fichier.'">Show file</a></li>
+                      <li><a href="/data/COBRA_depot/'.$fichier.'">Show file</a></li>
                       <!--<li><a href="#" id="myHrefbuttonremove">Remove file</a></li>-->
                       <!--<li><a href="#">Remove</a></li>-->
                       <!--<li role="separator" class="divider"></li>
